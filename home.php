435078f467641e4d1abe1e5776a5244f71c610fb
Logeshkanna18 <131451895+Logeshkanna18@users.noreply.github.com>
2024-05-23 21:09:35 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,78 @@
+ 
+  <section id="call-to-action-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-sm-9">
+          <h1>job-harbor</h1>
+          <p>sea of opportunities</p>
+        </div>
+       <!--  <div class="col-md-2 col-sm-3">
+          <a href="#" class="btn btn-primary">Read More</a>
+        </div> -->
+      </div>
+    </div>
+  </section>
+  
+  <section id="content">
+  
+  
+  <div class="container">
+        <div class="row">
+      <div class="col-md-12">
+        <div class="aligncenter"><h2 class="aligncenter">Company</h2><!-- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus ovident, doloribus omnis minus temporibus perferendis nesciunt.. --></div>
+        <br/>
+      </div>
+    </div>
+
+    <?php 
+      $sql = "SELECT * FROM `tblcompany`";
+      $mydb->setQuery($sql);
+      $comp = $mydb->loadResultList();
+
+
+      foreach ($comp as $company ) {
+        # code...
+    
+    ?>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-building-o"></i>
+                <div class="info-blocks-in">
+                    <h3><?php echo $company->COMPANYNAME;?></h3>
+                    <!-- <p><?php echo $company->COMPANYMISSION;?></p> -->
+                    <p>Address :<?php echo $company->COMPANYADDRESS;?></p>
+                    <p>Contact No. :<?php echo $company->COMPANYCONTACTNO;?></p>
+                </div>
+            </div>
+
+    <?php } ?> 
+  </div>
+  </section>
+  
+  <section class="section-padding gray-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-title text-center">
+            <h2 >Popular Jobs</h2>  
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 ">
+          <?php 
+            $sql = "SELECT * FROM `tblcategory`";
+            $mydb->setQuery($sql);
+            $cur = $mydb->loadResultList();
+
+            foreach ($cur as $result) {
+              echo '<div class="col-md-3" style="font-size:15px;padding:5px">* <a href="'.web_root.'index.php?q=category&search='.$result->CATEGORY.'">'.$result->CATEGORY.'</a></div>';
+            }
+
+          ?>
+        </div>
+      </div>
+ 
+    </div>
+  </section>    
+  
+  
